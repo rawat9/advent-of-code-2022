27e93ed5c9b4ab3550857793115d73d04b310eb1
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-12-08 07:09:45 +0000

    add working solution day-07

--- day-07/solution.py
+++ day-07/solution.py
@@ -1,82 +1,58 @@
 from pprint import pprint
-isDirectory = lambda x: x.startswith("dir")
-isCommand = lambda x: x.startswith("$")
+
+def get_input(path: str) -> list[str]:
+    with open(path, mode="r") as file:
+        terminal_output = file.read().split("\n")
+
+    return terminal_output
+
 
 stack = []
-dir_tree = {}
+directory_tree = {}
 
-def find_file_size(files):
-    return sum(map(lambda x: int(x.split()[0]), list(filter(lambda x: x.split()[0] != 'dir', files))))
+isCommand = lambda x: x.startswith("$")
+
 
 def get_cwd() -> str:
     return stack[-1]
-    # return " /".join(stack)
 
-def build_dir_tree(terminal_output: list[str]) -> None:
-    ls_output = []
+def build_dir_tree(terminal_output: list[str]):
     for line in terminal_output:
         if isCommand(line):
             command, *args = line.split()[1:]
 
             if command == "cd":
                 dir = args[0]
-                if dir == '..':
+                if dir == "..":
                     stack.pop()
                 else:
                     stack.append(dir)
-                ls_output = []
-
-            elif command == "ls":
-                pwd = get_cwd()
-                dir_tree[pwd] = ls_output
+                    directory_tree[dir] = []
         else:
-            ls_output.append(line)
+            pwd = get_cwd()
+            a, b = line.split()
+            if a == 'dir':
+                directory_tree[pwd].append(b)
+            else:
+                directory_tree[pwd].append(int(a))
 
-    
-    for files in dir_tree.values():
-        filesize = find_file_size(files)
-        files.append(filesize)
+def get_sum(directory):
+    total = 0
 
-    return dir_tree
+    for file in directory_tree[directory]:
+        if isinstance(file, str):
+            total += get_sum(file)
+        else:
+            total += file
+    return total
 
-def get_input(path: str) -> list[str]:
-    with open(path, mode="r") as file:
-        terminal_output = file.read().split("\n")
 
-    return terminal_output[:-1]
+def find_right_file_size():
+    return sum(map(lambda x: get_sum(x) if get_sum(x) <= 100000 else 0, directory_tree.keys()))
 
 
 if __name__ == "__main__":
     terminal_output = get_input("day-07/test.txt")
-    pprint(build_dir_tree(terminal_output))
-
-    """
-    To keep track of current working directory
-    stack = [/, d]
-    ------------------------------------------
-    '/': [
-            dir a: 
-                [dir e: [584 i], 29116 f, 2557 g, 62596 h.lst], 
-            14848514 b.txt, 
-            8504156 c.dat, 
-            dir d: [
-                4060174 j,
-                8033020 d.log,
-                5626152 d.ext,
-                7214296 k
-            ]
-        ]
-    ----------------------------------------------
-    '/': [
-        dir a:
-        14848514 b.txt, 
-        8504156 c.dat, 
-        dir d: [
-            4060174 j,
-            8033020 d.log,
-            5626152 d.ext,
-            7214296 k
-        ]
-
-    ]
-    """
+    build_dir_tree(terminal_output)
+    pprint(directory_tree)
+    pprint(find_right_file_size())
