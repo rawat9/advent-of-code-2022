4c27770febca9d7c70c8ed729ceec1228cd48846
Anurag <anurag.rawat0201@gmail.com>
2022-12-08 20:16:28 +0000

    complete day-08 challenge

---
+++ day-08/solution.py
@@ -0,0 +1,68 @@
+from pprint import pprint
+
+
+def get_input(path: str):
+    with open(path, mode="r") as file:
+        trees = [list(map(int, line)) for line in file.read().split()]
+
+    return trees
+
+
+def trees_below(row, col, matrix):
+    return [r[col] for r in matrix[row + 1 :]]
+
+
+def trees_above(row, col, matrix):
+    return [r[col] for r in matrix[:row]]
+
+
+def get_distance(trees, current):
+    d = 0
+    for t in trees:
+        d += 1
+        if t >= current:
+            break
+
+    return d
+
+
+def get_visibility_count_and_highest_scenic_score(matrix):
+    width = 2 * len(matrix[0])
+    height = 2 * (len(matrix) - 2)
+    visible = width + height
+    highest_score = 0
+
+    for row in range(1, len(matrix) - 1):
+        for col in range(1, len(matrix[0]) - 1):
+            current = matrix[row][col]
+
+            left = matrix[row][:col]
+            right = matrix[row][col + 1 :]
+            bottom = trees_below(row, col, matrix)
+            top = trees_above(row, col, matrix)
+
+            if any(
+                [
+                    all(tree < current for tree in left),
+                    all(tree < current for tree in right),
+                    all(tree < current for tree in bottom),
+                    all(tree < current for tree in top),
+                ]
+            ):
+                visible += 1
+
+                scenic_score = get_distance(left[::-1], current) * get_distance(
+                    right, current
+                ) * get_distance(top[::-1], current) * get_distance(bottom, current)
+
+                if scenic_score > highest_score:
+                    highest_score = scenic_score
+
+    return visible, highest_score
+
+
+if __name__ == "__main__":
+    trees = get_input("day-08/input.txt")
+    visiblility_count, scenic_score = get_visibility_count_and_highest_scenic_score(trees)
+    print('PART 1', visiblility_count)
+    print('PART 2', scenic_score)
